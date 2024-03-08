6889c259c14a6c5a0502c3aeecd963894e4a8910
Casvdt <302284632@student.rocmondriaan.nl>
2024-03-08 14:25:47 +0100

    UPD

--- Sports/Formula1/formula.js
+++ Sports/Formula1/formula.js
@@ -2,24 +2,27 @@ fetch('formula1.json')
     .then(response => response.json())
     .then(jsonData => poep(jsonData));
 
-    function poep(myPoep) {
+function poep(myPoep) {
 
-        const listContainer = document.querySelector(".listing");
-    
-        for (let i = 0; i < myPoep.length; i++) {
+    const listContainer = document.querySelector(".listing");
+
+    for (let i = 0; i < myPoep.length; i++) {
         listContainer.innerHTML += ` <li>
         <h2>${myPoep[i].title}</h2>
         <div class="body">
         <img class="imageUrl" src = "${myPoep[i].imageUrl}">
+        <div class="drivers">
+        <img class="imageUrl1" src = "${myPoep[i].firstDriverImg}">
+        <img class="imageUrl1" src = "${myPoep[i].secondDriverImg}">
         </div>
-        <h4>${myPoep[i].drivers}</h4>
-        <p>${myPoep[i].description}</p>
+        </div>
+        <h4 class="tekst">${myPoep[i].drivers}</h4>
+        <p class="tekst">${myPoep[i].description}</p>
 
         <div class="cta">
             <a href="${myPoep[i].websiteUrl}">klik hier!</a>
         </div>
         </li>`
-        
+
     }
-    }
-    
+}
