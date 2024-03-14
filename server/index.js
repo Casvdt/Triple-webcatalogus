f0c136dac0bf541e7a76d43aa388ecb0ed54da5c
S den Hollander <302045198@student.rocmondriaan.nl>
2024-03-14 15:48:15 +0100

    upd

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
 
@@ -16,4 +17,14 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
+app.get("/epicgames", (req, res) => {
+  res.setHeader("content-type", "application/json;charset=UTF-8");
+  fs.readFile("epic.json", "utf8", (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Internal Server Error');
+          return;
+      }
+      res.send(data);
+  });
+});
