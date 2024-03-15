814d1c1b84675e1557056ce0949b02a7127fa204
Casvdt <302284632@student.rocmondriaan.nl>
2024-03-15 15:28:46 +0100

    UPD

--- Sports/Football/football.js
+++ Sports/Football/football.js
@@ -10,11 +10,9 @@ function poep(myPoep) {
         <h2>${myPoep[i].competition_name}</h2>
         <div class="body">
         <img class="imageUrl" src = "${myPoep[i].imageUrl}">
-        <div class="drivers">
-        <img class="imageUrl1" src = "${myPoep[i].p}">
+        </div>
+        <img class="imageUrl1" src = "${myPoep[i].firstDriverImg}">
         <img class="imageUrl1" src = "${myPoep[i].secondDriverImg}">
-        </div>
-        </div>
          <h4 class="tekst1">${myPoep[i].popular_team}</h4>
         <p class="tekst">${myPoep[i].team_stadium}</p>
         <div class="cta">
