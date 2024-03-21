8748eafba31e712bf948f4f294132db87b6fc105
S den Hollander <302045198@student.rocmondriaan.nl>
2024-03-21 12:54:50 +0100

    upd

--- server/index.js
+++ server/index.js
@@ -39,3 +39,14 @@ app.get("/epicgames", (req, res) => {
           res.send(data);
       });
         });
+        app.get("/riot", (req, res) => {
+          res.setHeader("content-type", "application/json;charset=UTF-8");
+          fs.readFile("riot.json", "utf8", (err, data) => {
+              if (err) {
+                  console.error(err);
+                  res.status(500).send('Internal Server Error');
+                  return;
+              }
+              res.send(data);
+          });
+            });
