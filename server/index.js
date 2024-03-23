9bc29af23d78cfdbd0803c16b45541c19e331304
S den Hollander <302045198@student.rocmondriaan.nl>
2024-03-23 19:22:59 +0100

    upd

--- server/index.js
+++ server/index.js
@@ -54,5 +54,17 @@ app.get("/epicgames", (req, res) => {
               res.send(data);
           });
             });
+            app.get("/rockstar", (req, res) => {
+              res.setHeader("content-type", "application/json;charset=UTF-8");
+              fs.readFile("rockstar.json", "utf8", (err, data) => {
+                  if (err) {
+                      console.error(err);
+                      res.status(500).send('Internal Server Error');
+                      return;
+                  }
+                  res.send(data);
+              });
+                });
+    
 
 
