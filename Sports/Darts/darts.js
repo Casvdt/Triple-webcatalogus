27c9ffc4c784b2fb3c8bf3af473e6958591c18fb
Casvdt <302284632@student.rocmondriaan.nl>
2024-03-24 19:49:28 +0100

    UPD

--- Sports/Darts/darts.js
+++ Sports/Darts/darts.js
@@ -12,14 +12,13 @@ function poep(myPoep) {
         <div class="body">
         <img class="imageUrl" src = "${myPoep[i].player_img}">
         <div class="drivers">
-        <img class="imageUrl1" src = "${myPoep[i].firstDriverImg}">
-        <img class="imageUrl1" src = "${myPoep[i].secondDriverImg}">
+        <img class="imageUrl1" src = "${myPoep[i].dartboard_img}">
         </div>
         </div>
-         <h4 class="tekst1">${myPoep[i].driver1 + " -- " + myPoep[i].driver2}</h4>
+         <h4 class="tekst1">World ranking: ${myPoep[i].world_ranking}</h4>
         <p class="tekst">${myPoep[i].description}</p>
         <div class="cta">
-            <a href="${myPoep[i].websiteUrl}">Click here for the website!</a>
+            <a href="${myPoep[i].wikipedialink}">Click here for the wikipedia!</a>
         </div>
         </li>`
     }
--- Sports/Football/football.js
+++ Sports/Football/football.js
@@ -15,7 +15,7 @@ function poep(myPoep) {
          <h4 class="tekst1">Popular team: ${myPoep[i].popular_team}</h4>
          <img class="club" src ="${myPoep[i].club_img}">
          <p class="tekst1"> ${myPoep[i].description}</p>
-        <p class="tekst">Stadion: ${myPoep[i].team_stadium}</p>
+        <p class="tekst">Stadium: ${myPoep[i].team_stadium}</p>
         <div class="cta">
             <a href="${myPoep[i].websiteUrl}">Click here for the website!</a>
         </div>
